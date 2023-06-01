9ec055331c83bc82f170db78334b725c7b5533e8
Vadya <kosmat3936@gmail.com>
2023-06-01 21:40:11 +0300

    adding user authentication

---
+++ app/Http/Controllers/Auth/SocialController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use GuzzleHttp\Client;
+use App\Services\SocialService;
+use Auth;
+
+
+class SocialController extends Controller
+{
+    private $client;
+    private $socialService;
+    public function __construct(Client $client, SocialService $socialService){
+        $this->client = new Client([
+            'verify' => storage_path('app/ca-bundle.crt'),
+        ]);
+        $this->socialService = $socialService;
+    }
+
+    public function index(){
+        return Socialite::driver('vkontakte')->redirect();
+    }
+
+    public function callback(){
+        $userSocialite = Socialite::driver('vkontakte')
+            ->setHttpClient($this->client)
+            ->user();
+        if($user = $this->socialService->save($userSocialite)){
+            Auth::login($user);
+            return redirect()->route('home');
+        }
+        return back(400); 
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\HomeController,
-    App\Http\Controllers\Admin\DictationController
+    App\Http\Controllers\Admin\DictationController,
+    App\Http\Controllers\Auth\SocialController
 ;
 
 /*
@@ -23,4 +24,13 @@ Route::get('/', function () {
 Route::prefix('admin')->group(function(){
     Route::get('/', [HomeController::class, 'index']);
     Route::resource('/dictation', DictationController::class);
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => 'guest'], function(){
+    Route::get('/vk/auth', [SocialController::class, 'index'])->name('vk.auth');
+    Route::get('/vk/auth/callback', [SocialController::class, 'callback']);
 });

a62b148c8c948e75fdda5ec77b24e4912be328d1
Vadya <kosmat3936@gmail.com>
2023-06-01 21:32:08 +0300

    changing methods for creating and updating the dictation

--- app/Repositories/DictationRepository.php
+++ app/Repositories/DictationRepository.php
@@ -25,23 +25,16 @@ class DictationRepository{
     }
 
     public function createDictation($data){
-        $newDictation = new $this->dictation;
-        $newDictation->fill([
-            'title' => $data['title'],
-            'video_link' => $data['video_link'],
-            'is_active' => $data['is_active'],
-            'description' => $data['description'],
-            'start_date_time' => $data['start_date_time'],
-            'end_date_time' => $data['end_date_time'],
-        ]);
-        $newDictation->save();
-        return $newDictation->fresh();
+        $newDictation = $this->dictation->create($data);
+        return $newDictation;
     }
 
     public function updateDictation($data, $id){
         $dictationChange = $this->dictation
             ->where('id', $id)
-            ->update($data);
+            ->first()
+            ->fill($data);
+        $dictationChange->save();
         return $dictationChange;
     }
 

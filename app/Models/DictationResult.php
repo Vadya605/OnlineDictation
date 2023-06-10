aa69b2dd7c985f930e089046d00b54d4af43b507
Vadya <kosmat3936@gmail.com>
2023-06-10 16:21:01 +0300

    Determining the relationships in the model of dictation results

--- app/Models/DictationResult.php
+++ app/Models/DictationResult.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class DictationResult extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function dictation()
+    {
+        return $this->belongsTo(Dictation::class);
+    }
 }

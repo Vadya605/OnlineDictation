37a1094a613c39c53eff1d3bdeef0189b1d87d7f
Vadya <kosmat3936@gmail.com>
2023-06-16 19:02:01 +0300

    Correction page title

--- resources/views/layouts/adminApp.blade.php
+++ resources/views/layouts/adminApp.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Админ панель</title>
     @vite(['resources/sass/app.scss', 'resources/css/adminApp.css', 'resources/js/app.js'])
 </head>
 <body>

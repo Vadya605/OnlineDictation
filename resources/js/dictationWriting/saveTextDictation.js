93410938314cec6c5e3449459e25ce695f12eb84
Vadya <kosmat3936@gmail.com>
2023-06-22 18:11:40 +0300

    Adding user id and dictation id when saving dictation text to local storage

--- resources/js/dictationWriting/saveTextDictation.js
+++ resources/js/dictationWriting/saveTextDictation.js
@@ -1,9 +1,11 @@
-const textResult = document.querySelector('#text_result');
+const textResult = document.querySelector('#text_result')
+const userId = document.querySelector('#user_id').value
+const dictationId = document.querySelector('#dictation_id').value
 
 textResult.addEventListener('input', function(){
-    localStorage.setItem('textResult', this.value);
+    localStorage.setItem(`textResult_${userId}_${dictationId}`, this.value)
 })
 
 window.addEventListener('load', function(){
-    textResult.value = localStorage.getItem('textResult')
+    textResult.value = localStorage.getItem(`textResult_${userId}_${dictationId}`)
 })

dc32946593fdaf7b0ae0d627d40bc6508c1522ad
Vadya <kosmat3936@gmail.com>
2023-07-06 15:46:23 +0300

    Display the selected dictation and the user after filtering the dictation results

--- resources/js/admin/dictationResult/initAutoCompleteSearch.js
+++ resources/js/admin/dictationResult/initAutoCompleteSearch.js
@@ -3,45 +3,78 @@ select2()
 
 const searchDictaitonPath = "/admin/dictation/autoCompleteSearch";
 const searchUserPath = "/admin/user/autoCompleteSearch";
+const url = new URL(window.location.href)
+
+const dictationSearch = $('#dictationSearch')
+const userSearch = $('#userSearch')
+
+const selectedDictationId = url.searchParams.get('dictation')
+const selectedUserId = url.searchParams.get('user')
 
 
-$('#dictationSearch').select2({
+dictationSearch.select2({
     placeholder: 'Диктант',
     ajax: {
         url: searchDictaitonPath,
         dataType: 'json',
         processResults: function (data) {
-        return {
-            results:  $.map(data, function (item) {
-                return {
-                    text: item.title,
-                    id: item.id,
-                }
-            })
-        };
+            return {
+                results:  $.map(data, function (item) {
+                    return {
+                        text: item.title,
+                        id: item.id,
+                    }
+                })
+            }
         },
         cache: true
     }
 });
 
-$('#userSearch').select2({
+$.ajax({
+    url: searchDictaitonPath,
+    dataType: 'json',
+    success: function (data) {
+        const selectedItem = data.find(item => item.id == selectedDictationId)
+
+        if (selectedItem) {
+            const option = new Option(selectedItem.title, selectedItem.id);
+            dictationSearch.append(option).trigger('change');
+        }
+    },
+});
+
+userSearch.select2({
     placeholder: 'Пользователь',
     ajax: {
         url: searchUserPath,
         dataType: 'json',
         processResults: function (data) {
-        return {
-            results:  $.map(data, function (item) {
-                return {
-                    text: item.name,
-                    id: item.id,
-                }
-            })
-        };
+            return {
+                results:  $.map(data, function (item) {
+                    return {
+                        text: item.name,
+                        id: item.id,
+                    }
+                })
+            }
         },
         cache: true
     }
 });  
+
+$.ajax({
+    url: searchUserPath,
+    dataType: 'json',
+    success: function (data) {
+        const selectedItem = data.find(item => item.id == selectedUserId)
+
+        if (selectedItem) {
+            const option = new Option(selectedItem.name, selectedItem.id);
+            userSearch.append(option).trigger('change');
+        }
+    },
+});
 
 
 

fa106be994aa92fdf09d170aec67c988f5ef0554
Vadya <kosmat3936@gmail.com>
2023-08-17 22:00:32 +0300

    Fix user to admin check

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use App\Enums\User\UserRole;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdminMiddleware
@@ -15,7 +16,7 @@ class AdminMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if($request->user() && $request->user()->role === 'admin'){
+        if($request->user() && $request->user()->role === UserRole::ADMIN){
             return $next($request);
         }
         
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use App\Enums\User\UserRole;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -23,7 +24,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Gate::define('access-admin-panel', function ($user) {
-            return $user->role === 'admin';
+            return $user->role === UserRole::ADMIN;
         });
     }
 }
